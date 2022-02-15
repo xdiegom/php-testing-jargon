b7adf82629c012b357e4f741dd3b99364a01f63a
Diego Melgar <diego.melgar.sv@gmail.com>
2022-02-15 11:46:53 -0600

    Setup the World

--- tests/TagParserTest.php
+++ tests/TagParserTest.php
@@ -5,13 +5,20 @@ namespace Tests;
 use App\TagParser;
 use PHPUnit\Framework\TestCase;
 
+// Given, When, Then
+// Arrange, Act, Assert
 class TagParserTest extends TestCase
 {
+    protected $parser;
+
+    protected function setup(): void
+    {
+        $this->parser = new TagParser;
+    }
+
     public function test_it_parses_a_single_tag()
     {
-        $parser = new TagParser;
-
-        $result = $parser->parse('personal');
+        $result = $this->parser->parse('personal');
         $expected = ['personal'];
 
         $this->assertSame($expected, $result);
@@ -19,9 +26,7 @@ class TagParserTest extends TestCase
 
     public function test_it_parses_a_comma_separated_list_of_tags()
     {
-        $parser = new TagParser;
-
-        $result = $parser->parse('personal, money, family');
+        $result = $this->parser->parse('personal, money, family');
         $expected = ['personal', 'money', 'family'];
 
         $this->assertSame($expected, $result);
@@ -29,9 +34,7 @@ class TagParserTest extends TestCase
 
     public function test_it_parses_a_pipe_separated_list_of_tags()
     {
-        $parser = new TagParser;
-
-        $result = $parser->parse('personal | money | family');
+        $result = $this->parser->parse('personal | money | family');
         $expected = ['personal', 'money', 'family'];
 
         $this->assertSame($expected, $result);
@@ -39,13 +42,11 @@ class TagParserTest extends TestCase
 
     public function test_spaces_are_optional()
     {
-        $parser = new TagParser;
-
-        $result = $parser->parse('personal,money,family');
+        $result = $this->parser->parse('personal,money,family');
         $expected = ['personal', 'money', 'family'];
         $this->assertSame($expected, $result);
 
-        $result = $parser->parse('personal|money|family');
+        $result = $this->parser->parse('personal|money|family');
         $expected = ['personal', 'money', 'family'];
         $this->assertSame($expected, $result);
     }
